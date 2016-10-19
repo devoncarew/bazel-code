8816aa8f8ec7637d4c3b6dabafaab4fd38dc39b2
Devon Carew <devoncarew@google.com>
2016-10-18 23:12:26 -0700

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    // console.log('Hello, bazel!');
+}
+
+export function deactivate() {
+
+}
